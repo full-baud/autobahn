2a9932b004ca79005050d9411616a3cc7785ce0a
achingbrain <alex@achingbrain.net>
2011-03-12 18:48:17 +0000

    sdfhjasdfjasfoijasdfoijsadf

--- server/server.js
+++ server/server.js
@@ -1,22 +1,80 @@
 var sys = require('sys'),
      ws = require('ws')
 
-var server = ws.createServer(function (socket) {
+Player = {
+	// generated, unique.  not sequential.
+	token: "",
+	
+	// user entered
+	name: "",
+	
+	// eg. MD5'd email for gravatar
+	avatar: ""
+}
 
-  socket.addListener("connect", function (resource) {
-    sys.puts("client connected from " + resource);
-    
-	moveIt();
-  })
+Lounge = {
+	numPlayers: 4,
+	title: "Lounge",
+	players: [
+	
+	]
+};
 
-  socket.addListener("data", function (data) {
-    socket.write(data);
-  })
+Game = {
+	
+};
 
-  socket.addListener("close", function () {
-    sys.puts("client left");
-  })
-  
+var autoBahn = {
+	// a lounge is a game that hasn't started yet
+	lounges: [
+		
+	],
+	
+	// games are in progress
+	games: [
+		
+	],
+	
+	getLoungeList: function() {
+		sys.puts("asked for lounge list");
+		
+		return autoBahn.lounges;
+	}
+};
+
+var server = ws.createServer(function(socket) {
+	socket.addListener("connect", function(resource) {
+		sys.puts("client connected from " + resource);
+	});
+	
+	socket.addListener("data", function (data) {
+		sys.puts("got " + data);
+		
+		//{action: "methodName", args: [bar, baz]}
+		
+		try {
+			var methodCall = eval("(" + data + ")");
+			//var methodCall = JSON.parse(data);
+		} catch(e) {
+			sys.puts(":( " + e);
+		}
+		
+		if(methodCall && autoBahn[methodCall.action]) {
+			var output = {
+				action: methodCall.action,
+				args: methodCall.args,
+				response: autoBahn[methodCall.action].apply(this, methodCall.args ? methodCall.args : [])
+			}
+			
+			socket.write(JSON.stringify(output)+ "\r\n");
+		}
+	});
+	
+	socket.addListener("close", function () {
+		sys.puts("client left");
+	});
+	
+	
   var moveIt = function() {
   	var direction = Math.ceil(Math.random());
   	var amount = Math.random() * 10;
@@ -33,7 +91,4 @@ var server = ws.createServer(function (socket) {
     setTimeout(moveIt, movementTime);
   }
  
-})
-
-server.listen(8080)
-
+}).listen(8080);
