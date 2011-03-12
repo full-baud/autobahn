cb48f2938fcaf2f271f41c188d02946569ab032d
davidtron <david@spiraltechnology.co.uk>
2011-03-12 17:19:21 +0000

    Server is now sending JSON object with direction and velocity

--- server/server.js
+++ server/server.js
@@ -4,20 +4,36 @@ var sys = require('sys'),
 var server = ws.createServer(function (socket) {
 
   socket.addListener("connect", function (resource) {
-    sys.puts("client connected from " + resource)
+    sys.puts("client connected from " + resource);
     
-    socket.write("welcome\r\n")
+	moveIt();
   })
 
   socket.addListener("data", function (data) {
-    socket.write(data)
+    socket.write(data);
   })
 
   socket.addListener("close", function () {
-    sys.puts("client left")
+    sys.puts("client left");
   })
   
+  var moveIt = function() {
+  	var direction = Math.ceil(Math.random());
+  	var amount = Math.random() * 10;
+  	
+  	var stuff = {
+  		axis: direction,
+  		distance: amount
+  	};
+  	
+  	sys.puts("Sending to client " + JSON.stringify(stuff));
+  	
+  	socket.write(JSON.stringify(stuff)+ "\r\n");
+  	var movementTime = Math.random() * 1000;
+    setTimeout(moveIt, movementTime);
+  }
  
 })
 
 server.listen(8080)
+
